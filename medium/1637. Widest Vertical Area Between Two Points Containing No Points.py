adc408594dbd5e9282a2e3554fbe41a9fbf79653
JakubKosakowski <67673985+JakubKosakowski@users.noreply.github.com>
2023-06-05 19:01:03 +0200

    Create 1637. Widest Vertical Area Between Two Points Containing No Points.py
    Runtime: 906 ms
    Memory: 59.1 MB

---
+++ medium/1637. Widest Vertical Area Between Two Points Containing No Points.py
@@ -0,0 +1,7 @@
+class Solution:
+    def maxWidthOfVerticalArea(self, points: List[List[int]]) -> int:
+        ans = 0
+        points.sort(key=lambda x: x[0])
+        for i in range(len(points)-1):
+            ans = max(ans, (points[i+1][0] - points[i][0]))
+        return ans
